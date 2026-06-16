b18983e56dff3f6ab58b7a575bb2bfa724690dce
Ruwani Sugandika <sugandikarh@gmail.com>
2026-06-16 12:52:31 +0530

    initial project structure - four spring boot services generated

---
+++ product-service/src/test/java/com/sugandi/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugandi.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
